86b586e9b9c2fa3d1405ac05e0c2dae60878e9cc
Karol Majka <karol.x.majka@gsk.com>
2017-05-12 19:51:27 +0200

    add TableView to project

---
+++ Groby Poznanskie API/GraveTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  GraveTableViewCell.swift
+//  Groby Poznanskie API
+//
+//  Created by Karol Majka on 12/05/2017.
+//  Copyright © 2017 karolmajka. All rights reserved.
+//
+
+import UIKit
+
+class GraveTableViewCell: UITableViewCell {
+
+    @IBOutlet var name: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
